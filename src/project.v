e6f4b3b29d1144617a9e487606d8aafe3ef94cf5
jay <jaygraham.mt@gmail.com>
2026-04-27 01:34:09 -0600

    fixing top level wrapper

--- src/project.v
+++ src/project.v
@@ -1,27 +1,41 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2026 Jay Graham
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module tt_um_lutram4 (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n 
 );
+    wire ccff_tail;
+    wire out;
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    lutram4 dut (
+        .clk(clk),
+        .rst(rst_n),
+        .config_done(ui_in[0]),
+        .we(ui_in[1]),
+        .wdata(ui_in[2]),
+        .ccff_head(ui_in[3]),
+        .in(ui_in[7:4]),
+        .ccff_tail(ccff_tail),
+        .out(out)
+    );
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    assign uo_out[0]   = ccff_tail;
+    assign uo_out[1]   = out;
+    assign uo_out[7:2] = 0;
+    assign uio_out     = 0;
+    assign uio_oe      = 0;
+
+    wire _unused = &{ena, uio_in, 1'b0};
 
 endmodule
